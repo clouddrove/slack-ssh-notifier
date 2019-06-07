d183de09179952eed58eb961c90a04544b6ee681
Nikita Dugar <nikita@clouddrove.com>
2019-06-07 18:39:02 +0530

    update variable and write comment

--- ssh_alert.sh
+++ ssh_alert.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#Please add Chaneel Name and Webhook URL of slack here to send Slack Notification
 
 channel="CHANEEL_NAME"
+webhook_url="WEBHOOK_URL_HERE"
+
 if [[ $channel == "" ]]
 then
         echo "No channel specified"
@@ -8,7 +11,6 @@ then
 fi
 
 shift
-
 host="`hostname`"
 
 content="\"attachments\": [ { \"mrkdwn_in\": [\"text\", \"fallback\"], \"fallback\": \"SSH login: $USER connected to \`$host\`\", \"text\": \"SSH login to \`$host\`\", \"fields\": [ { \"title\": \"User\", \"value\": \"$PAM_USER\", \"short\": true }, { \"title\": \"IP Address\", \"value\": \"$PAM_RHOST\", \"short\": true }, { \"title\": \"Date\", \"value\": \"`date`\", \"short\": true } ], \"color\": \"#F35A00\" } ]"
@@ -17,9 +19,8 @@ then
         echo "No text specified"
         exit 1
 fi
-url="WEBHOOK_URL_HERE"
 if [ ${PAM_TYPE} = "open_session" ]; then
-	curl -s -X POST --data-urlencode "payload={\"channel\": \"$channel\", \"mrkdwn\": true, \"username\": \"ssh-bot\", $content, \"icon_emoji\": \":computer:\"}" $url
+	curl -s -X POST --data-urlencode "payload={\"channel\": \"$channel\", \"mrkdwn\": true, \"username\": \"ssh-bot\", $content, \"icon_emoji\": \":computer:\"}" $webhook_url
 fi
 
 exit 0
